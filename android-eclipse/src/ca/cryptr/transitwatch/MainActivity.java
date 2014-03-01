7f2d683729df17b9883766a68d0b59bd1a511d77
Eugene Cheung <ey.cheung@outlook.com>
2014-03-01 00:00:04 -0500

    Initial layouts code.

--- android-eclipse/src/ca/cryptr/transitwatch/MainActivity.java
+++ android-eclipse/src/ca/cryptr/transitwatch/MainActivity.java
@@ -1,8 +1,11 @@
 package ca.cryptr.transitwatch;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -14,9 +17,32 @@ public class MainActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_settings:
+			showActivity(SettingsActivity.class);
+			return true;
+		default:
+			return false;
+		}
+	}
+
+	public void addStop(View view) {
+		showActivity(AddTransitActivity.class);
+	}
+
+	/**
+	 * Starts and displays the specified activity.
+	 * @param activity The activity to display.
+	 */
+	public void showActivity(Class<?> activity) {
+		Intent intent = new Intent(this, activity);
+		startActivity(intent);
 	}
 
 }
