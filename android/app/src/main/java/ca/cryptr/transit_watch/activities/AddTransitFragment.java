75a901e5c2217f1c1b0b330fb5202bec31723cbc
Eugene Cheung <ey.cheung@outlook.com>
2014-03-01 16:24:57 -0500

    Transit page shows transits.

--- android/app/src/main/java/ca/cryptr/transit_watch/activities/AddTransitFragment.java
+++ android/app/src/main/java/ca/cryptr/transit_watch/activities/AddTransitFragment.java
@@ -2,24 +2,60 @@ package ca.cryptr.transit_watch.activities;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import ca.cryptr.transit_watch.R;
 
 public class AddTransitFragment extends Fragment {
 
+    private ListView transits;
+    private EditText filter;
+    private SimpleAdapter adapter;
+
+    private View view;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_add_transit, container, false);
+        view = inflater.inflate(R.layout.fragment_add_transit, container, false);
 
         // Change title, menu items
         getActivity().getActionBar().setTitle(R.string.title_activity_add_transit);
         setHasOptionsMenu(true);
+
+        // Show the transit companies in the list
+        setupTransitList();
+
+        filter = (EditText) view.findViewById(R.id.filter_transit);
+        filter.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+                adapter.getFilter().filter(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+            }
+        });
 
         return view;
     }
@@ -31,6 +67,28 @@ public class AddTransitFragment extends Fragment {
         menu.findItem(R.id.action_add_next_stop).setVisible(false);
         menu.findItem(R.id.action_add_previous_route).setVisible(false);
         menu.findItem(R.id.action_add_done).setVisible(false);
+    }
+
+    public void setupTransitList() {
+        transits = (ListView) view.findViewById(R.id.add_transit_list);
+
+        String[] companies = getResources().getStringArray(R.array.transits);
+
+        List<Map<String, String>> data = new ArrayList<Map<String, String>>();
+
+        for (int i = 0; i < companies.length; i+=2) {
+            Map<String, String> item = new HashMap<String, String>();
+            item.put("1", companies[i]);
+            item.put("2", companies[i+1]);
+            data.add(item);
+        }
+
+        adapter = new SimpleAdapter(getActivity(), data,
+                R.layout.add_list_item,
+                new String[] {"1", "2"},
+                new int[] {R.id.text1, R.id.text2});
+
+        transits.setAdapter(adapter);
     }
 
 }
