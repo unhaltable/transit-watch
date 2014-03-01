6034854893a05c0c0e3789fcd8846fb44560bafe
Eugene Cheung <ey.cheung@outlook.com>
2014-03-01 00:22:50 -0500

    All activities link together now.

--- android-eclipse/src/ca/cryptr/transitwatch/AddRouteActivity.java
+++ android-eclipse/src/ca/cryptr/transitwatch/AddRouteActivity.java
@@ -1,10 +1,11 @@
 package ca.cryptr.transitwatch;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
 
 public class AddRouteActivity extends Activity {
 
@@ -36,14 +37,11 @@ public class AddRouteActivity extends Activity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
 			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		case R.id.action_route_next:
+			Intent intent = new Intent(this, AddStopActivity.class);
+			startActivity(intent);
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
--- android-eclipse/src/ca/cryptr/transitwatch/AddStopActivity.java
+++ android-eclipse/src/ca/cryptr/transitwatch/AddStopActivity.java
@@ -1,10 +1,10 @@
 package ca.cryptr.transitwatch;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
 
 public class AddStopActivity extends Activity {
 
@@ -36,14 +36,10 @@ public class AddStopActivity extends Activity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
 			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		case R.id.action_stop_done:
+
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
